532366d27da15338cc3f53b90625ade594cdf2ec
AlvinChuang <alvin870203@gapp.nthu.edu.tw>
2020-03-16 22:06:03 +0800

    Modified to analyze all stations (success)

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -26,14 +26,36 @@ with open(cwb_filename) as csvfile:
 
 # Retrive ten data points from the beginning.
 ####target_data = data[:10]
-target_data = list(filter(lambda item: item['station_id'] == 'C0K28', data))
+
+data = sorted(data, key=lambda i: i['station_id']) #sort data by station_id
+#print(data) #checking
+output_data = [] #the output
+while len(data) != 0 : #until the data is empty, which means that all the stations have been handled
+   target_station = data[0]['station_id'] #get first station in data as target
+   target_data = list(filter(lambda item: item['station_id'] == target_station, data)) #get all the record of the target station
+   #print(target_data) #checking
+
+   target_data = list(filter(lambda del_99: del_99['WDSD'] != '-99.000', target_data)) #delete the ones that WDSD=-99.000
+   target_data = list(filter(lambda del_99: del_99['WDSD'] != '-999.000', target_data)) #delete the ones that WDSD=-999.000
+
+   target_data = sorted(target_data, key=lambda i: i['WDSD']) #sort target_data by WDSD
+   #print(target_data) #checking
+   if len(target_data)>=2 :
+      output_data.append([target_station, float(target_data[-1]['WDSD']) - float(target_data[0]['WDSD'])])
+   else :
+      output_data.append([target_station, 'None'])
+
+   data = list(filter(lambda del_item: del_item['station_id'] != target_station, data)) #delete the target station from the data
+   #remaining data are stations that haven't been hadled yet
+print(output_data)
 
 #=======================================
 
 # Part. 4
 #=======================================
 # Print result
-if len(target_data) == 0 :
-    print('None')
-print(target_data)
+#if len(target_data) == 0 :
+#   print('None')
+#else :
+#   print(target_data)
 #========================================
